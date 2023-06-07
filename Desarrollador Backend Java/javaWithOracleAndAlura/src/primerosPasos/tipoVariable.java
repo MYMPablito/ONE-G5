7a7280937c81837e0f72c8a7f648db5b54dbf896
MYMPablito <pablo-mato@hotmail.com>
2023-06-07 02:30:52 -0300

    Agrego ejemplo iniciar y declarar una variable

---
+++ Desarrollador Backend Java/javaWithOracleAndAlura/src/primerosPasos/tipoVariable.java
@@ -0,0 +1,21 @@
+package primerosPasos;
+
+public class tipoVariable {
+	
+	public static void main(String[] args) {
+		
+		int edad = 32;
+		
+		System.out.println(edad);
+		
+		edad = 47;
+		
+		System.out.println(edad);
+		
+		edad = 40 + 41;
+		
+		System.out.println("Mi abuelo acaba de cumplir " + edad + " años." );
+		
+	}
+
+}
